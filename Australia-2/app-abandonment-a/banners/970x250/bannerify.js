d5814449bc1e1c86378b8c5f217d61b4757277b9
root-stone-x <98025096+root-stone-x@users.noreply.github.com>
2022-01-24 18:05:56 +0000

    Add files via upload

---
+++ Australia-2/app-abandonment-a/banners/970x250/bannerify.js
@@ -0,0 +1,31 @@
+(function () {
+	function onImagesLoaded(container, event) {
+		var images = container.getElementsByTagName("img");
+		var loaded = images.length;
+		for (var i = 0; i < images.length; i++) {
+			if (images[i].complete) {
+				loaded--;
+			}
+			else {
+				images[i].addEventListener("load", function() {
+					loaded--;
+					if (loaded == 0) {
+						event();
+					}
+				});
+			}
+			if (loaded == 0) {
+				event();
+			}
+		}
+	}
+	
+	onImagesLoaded(document.getElementById("b_102368"), function() {
+		var elements = document.getElementById("b_102368").getElementsByClassName('js-bnfy');
+		for (var i=0; i < elements.length; i++) {
+			elements[i].style.display = 'block';
+		}
+	});
+	document.getElementById('b_102368').addEventListener('click', function () {
+		window.open(window.clickTag);
+	});})();
